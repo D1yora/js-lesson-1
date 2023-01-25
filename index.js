c6503d226ef9d2268220d415de77b989a32f2f43
D1yora <dioora2o3@gmail.com>
2023-01-25 09:12:03 +0500

    basic js info

---
+++ index.js
@@ -0,0 +1,29 @@
+// About  JAVASCRIPT
+
+// invented by Brendan Each  in 1995
+// Livescript, ECMA
+
+// compiled by
+// 1. in html  2.online js compilers   3. node console
+
+// variables
+// let, var, const
+
+// Data  types  8ta
+// 1. Number
+// 2. Rigint
+// 3. String
+// 4. Boolean
+// 5. Null
+// 6. Undefined
+// 7. Symbols
+// 8. Objects
+// primitve & non-primitive
+
+// null bilan undefined
+//  null'da value yo'q
+//  undefined'da bor
+
+console.log(null == undefined); true
+console.log(isNaN(5 + null));  false
+console.log(isNaN(5 + undefined)); true
